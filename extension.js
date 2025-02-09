8779cb1e360a4881dbb4712d291266096bffe128
mrkrook2012 <meindertkrook@gmail.com>
2025-02-09 14:10:51 +0100

    ADD: Changing colors functionality, removed bugs and errors. Next is custom monitor and multi-monitor support

--- extension.js
+++ extension.js
@@ -10,44 +10,92 @@ const XrandrDimmer = GObject.registerClass(
         _init() {
             super._init(0.0, "Xrandr Dimmer", false);
 
-            // Create and add an icon
             let icon = new St.Icon({
                 icon_name: 'display-brightness-symbolic',
                 style_class: 'system-status-icon',
             });
             this.add_child(icon);
 
-            // Create menu item with brightness slider
+            this.brightness = 1.0;
+            this.red = 1.0;
+            this.green = 1.0;
+            this.blue = 1.0;
+
+            // Store sliders
+            this.sliders = {};
+
+            this._addSlider("Brightness", 0.1, 1.0, "brightness");
+            this._addSlider("Red", 0.1, 2.0, "red");
+            this._addSlider("Green", 0.1, 2.0, "green");
+            this._addSlider("Blue", 0.1, 2.0, "blue");
+
+            this._addResetButton();
+
+            this._applySettings();
+        }
+
+        _addSlider(label, min, max, key) {
             let sliderItem = new PopupMenu.PopupBaseMenuItem({ activate: false });
             let sliderLabel = new St.Label({
-                text: "Brightness",
+                text: label,
                 y_align: Clutter.ActorAlign.CENTER
             });
             sliderItem.actor.add_child(sliderLabel, { expand: false });
 
-            this._slider = new Slider.Slider(1.0);
-            this._slider.connect('notify::value', () => {
-                let brightness = Math.max(0.1, this._slider.value).toFixed(2);
-                this._setBrightness(brightness);
+            let slider = new Slider.Slider((this[key] - min) / (max - min));
+            slider.connect('notify::value', () => {
+                this[key] = Math.max(min, min + slider.value * (max - min)).toFixed(2);
+                this._applySettings();
+
             });
-            sliderItem.actor.add_child(this._slider.actor, { expand: true });
+
+            sliderItem.actor.add_child(slider.actor, { expand: true });
             this.menu.addMenuItem(sliderItem);
 
-            // Add the menu to the panel
-            Main.panel.addToStatusArea("xrandr-dimmer", this);
+            // Store slider reference
+            this.sliders[key] = { slider, min, max };
         }
 
-        _setBrightness(brightness) {
+        _addResetButton() {
+            let resetItem = new PopupMenu.PopupMenuItem("Reset to Default");
+            resetItem.connect('activate', () => {
+                this.brightness = 1.0;
+                this.red = 1.0;
+                this.green = 1.0;
+                this.blue = 1.0;
+
+                // Update sliders
+                for (let key of ["brightness", "red", "green", "blue"]) {
+                    let { slider, min, max } = this.sliders[key];
+                    slider.value = (this[key] - min) / (max - min);
+                }
+
+                // Force apply settings
+                GLib.idle_add(GLib.PRIORITY_DEFAULT, () => {
+                    this._applySettings();
+                    return GLib.SOURCE_REMOVE;
+                });
+            });
+            this.menu.addMenuItem(resetItem);
+        }
+
+        _applySettings() {
             try {
                 new Gio.Subprocess({
-                    argv: ['xrandr', '--output', 'eDP', '--brightness', brightness],
+                    argv: [
+                        'xrandr', '--output', 'eDP', '--brightness',
+                        String(this.brightness), '--gamma',
+                        `${String(this.red)}:${String(this.green)}:${String(this.blue)}`
+                    ],
                     flags: Gio.SubprocessFlags.STDOUT_SILENCE | Gio.SubprocessFlags.STDERR_SILENCE,
                 }).init(null);
             } catch (e) {
-                logError(e, "Failed to change brightness");
+                logError(e, "Failed to change display settings");
             }
         }
+
     }
+
 );
 
 let _indicator = null;
@@ -60,7 +108,6 @@ function enable() {
         Main.panel.addToStatusArea("xrandr-dimmer", _indicator);
     }
 }
-
 
 function disable() {
     if (_indicator) {
